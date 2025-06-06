3f547227eb0134eb9f53a059659432965e29e7cb
jasper <kisekinosedai0912@gmail.com>
2025-06-06 18:28:05 +0800

    finished the dynamic image rendering based on filter values & implemented DRY princple

--- src/components/ScaleImage.jsx
+++ src/components/ScaleImage.jsx
@@ -1,10 +1,13 @@
-export default function ScaleImage({ img, alt }) {
+export default function ScaleImage({ img, alt, position }) {
     return (
-        <img 
-            src={img}
-            alt={alt}
-            loading="lazy"
-            className="w-[450px] h-[220px] object-fit border border-[#262626] rounded-lg" 
-        />
+        <div className="flex flex-col items-center gap-2">
+            <span className="font-outfit text-[#262626]"><b>{position}</b></span>
+            <img 
+                src={img}
+                alt={alt}
+                loading="lazy"
+                className="w-[450px] h-[220px] object-fit border border-[#262626] rounded-lg" 
+            />
+        </div>
     );
 }
--- src/components/Scales.jsx
+++ src/components/Scales.jsx
@@ -11,130 +11,167 @@ import HarmonicMinorPos3 from '../assets/img/h-pos3.png';
 import HarmonicMinorPos4 from '../assets/img/h-pos4.png';
 import HarmonicMinorPos5 from '../assets/img/h-pos5.png';
 import HarmonicMinorPos6 from '../assets/img/h-pos6.png';
-import HarmonicMinorPos7 from '../assets/img/h-pos7.png';
+// import HarmonicMinorPos7 from '../assets/img/h-pos7.png';
+import NaturalMinorPos1 from '../assets/img/n-pos1.png';
+import NaturalMinorPos2 from '../assets/img/n-pos2.png';
+import NaturalMinorPos3 from '../assets/img/n-pos3.png';
+import NaturalMinorPos4 from '../assets/img/n-pos4.png';
+import NaturalMinorPos5 from '../assets/img/n-pos5.png';
+import MajorPentatonicPos1 from '../assets/img/mp-pos1.png';
+import MajorPentatonicPos2 from '../assets/img/mp-pos2.png';
+import MajorPentatonicPos3 from '../assets/img/mp-pos3.png';
+import MajorPentatonicPos4 from '../assets/img/mp-pos4.png';
+import MajorPentatonicPos5 from '../assets/img/mp-pos5.png';
+import MinorPentatonicPos1 from '../assets/img/mnp-pos1.png';
+import MinorPentatonicPos2 from '../assets/img/mnp-pos2.png';
+import MinorPentatonicPos3 from '../assets/img/mnp-pos3.png';
+import MinorPentatonicPos4 from '../assets/img/mnp-pos4.png';
+import MinorPentatonicPos5 from '../assets/img/mnp-pos5.png';
+import DiminishedPos1 from '../assets/img/d-pos1.png';
+import DiminishedPos2 from '../assets/img/d-pos2.png';
+import DiminishedPos3 from '../assets/img/d-pos3.png';
+import DiminishedPos4 from '../assets/img/d-pos4.png';
 
+// Scake props arrays
 const majorScale = [
-    {
-        img: MajorPos1,
-        alt: '1st position',
-    },
-    {
-        img: MajorPos2,
-        alt: '2nd position',
-    },
-    {
-        img: MajorPos3,
-        alt: '3rd position',
-    },
-    {
-        img: MajorPos4,
-        alt: '4th position',
-    },
-    {
-        img: MajorPos5,
-        alt: '5th position',
-    },
+    { img: MajorPos1, alt: "Major Scale Position 1", position: "Position 1" },
+    { img: MajorPos2, alt: "Major Scale Position 2", position: "Position 2" },
+    { img: MajorPos3, alt: "Major Scale Position 3", position: "Position 3" },
+    { img: MajorPos4, alt: "Major Scale Position 4", position: "Position 4" },
+    { img: MajorPos5, alt: "Major Scale Position 5", position: "Position 5" }
 ];
 
 const harmonicMinorScale = [
-    {
-        img: HarmonicMinorPos1,
-        alt: '1st position',
-    },
-    {
-        img: HarmonicMinorPos2,
-        alt: '2nd position',
-    },
-    {
-        img: HarmonicMinorPos3,
-        alt: '3rd position',
-    },
-    {
-        img: HarmonicMinorPos4,
-        alt: '4th position',
-    },
-    {
-        img: HarmonicMinorPos5,
-        alt: '5th position',
-    },
-    {
-        img: HarmonicMinorPos6,
-        alt: '6th position',
-    },
-    {
-        img: HarmonicMinorPos7,
-        alt: '7th position',
-    },
+    { img: HarmonicMinorPos1, alt: "Harmonic Minor Position 1", position: "Position 1" },
+    { img: HarmonicMinorPos2, alt: "Harmonic Minor Position 2", position: "Position 2" },
+    { img: HarmonicMinorPos3, alt: "Harmonic Minor Position 3", position: "Position 3" },
+    { img: HarmonicMinorPos4, alt: "Harmonic Minor Position 4", position: "Position 4" },
+    { img: HarmonicMinorPos5, alt: "Harmonic Minor Position 5", position: "Position 5" },
+    { img: HarmonicMinorPos6, alt: "Harmonic Minor Position 6", position: "Position 6" },
+    // { img: HarmonicMinorPos7, alt: "Harmonic Minor Position 7", position: "Position 7" }
 ];
 
-export default function ScalesPage() {
-    // Split major scale
-    const firstMajorRow = majorScale.slice(0, 3);
-    const secondMajorRow = majorScale.slice(3);
-    // Split harmonic minor scale
-    const firstHarmonicMinorRow = harmonicMinorScale.slice(0, 4);
-    const secondHarmonicMinorRow = harmonicMinorScale.slice(4);
+const naturalMinorScale = [
+    { img: NaturalMinorPos1, alt: "Natural Minor Position 1", position: "Position 1" },
+    { img: NaturalMinorPos2, alt: "Natural Minor Position 2", position: "Position 2" },
+    { img: NaturalMinorPos3, alt: "Natural Minor Position 3", position: "Position 3" },
+    { img: NaturalMinorPos4, alt: "Natural Minor Position 4", position: "Position 4" },
+    { img: NaturalMinorPos5, alt: "Natural Minor Position 5", position: "Position 5" }
+];
 
+const majorPentatonicScale = [
+    { img: MajorPentatonicPos1, alt: "Major Pentatonic Position 1", position: "Position 1" },
+    { img: MajorPentatonicPos2, alt: "Major Pentatonic Position 2", position: "Position 2" },
+    { img: MajorPentatonicPos3, alt: "Major Pentatonic Position 3", position: "Position 3" },
+    { img: MajorPentatonicPos4, alt: "Major Pentatonic Position 4", position: "Position 4" },
+    { img: MajorPentatonicPos5, alt: "Major Pentatonic Position 5", position: "Position 5" }
+];
+
+const minorPentatonicScale = [
+    { img: MinorPentatonicPos1, alt: "Minor Pentatonic Position 1", position: "Position 1" },
+    { img: MinorPentatonicPos2, alt: "Minor Pentatonic Position 2", position: "Position 2" },
+    { img: MinorPentatonicPos3, alt: "Minor Pentatonic Position 3", position: "Position 3" },
+    { img: MinorPentatonicPos4, alt: "Minor Pentatonic Position 4", position: "Position 4" },
+    { img: MinorPentatonicPos5, alt: "Minor Pentatonic Position 5", position: "Position 5" }
+];
+
+const diminishedScale = [
+    { img: DiminishedPos1, alt: "Diminished Position 1", position: "Position 1" },
+    { img: DiminishedPos2, alt: "Diminished Position 2", position: "Position 2" },
+    { img: DiminishedPos3, alt: "Diminished Position 3", position: "Position 3" },
+    { img: DiminishedPos4, alt: "Diminished Position 4", position: "Position 4" }
+];
+
+// Encapsulated filter element values in an object
+const scalesData = {
+    'major-scale': {
+        name: 'Major Scale',
+        images: majorScale,
+        showModes: true
+    },
+    'harmonic-minor-scale': {
+        name: 'Harmonic Minor Scale',
+        images: harmonicMinorScale,
+        showModes: false
+    },
+    'natural-minor-scale': {
+        name: 'Natural Minor Scale',
+        images: naturalMinorScale,
+        showModes: false
+    },
+    'major-pentatonic-scale': {
+        name: 'Major Pentatonic Scale',
+        images: majorPentatonicScale,
+        showModes: false
+    },
+    'minor-pentatonic-scale': {
+        name: 'Minor Pentatonic Scale',
+        images: minorPentatonicScale,
+        showModes: false
+    },
+    'diminished-scale': {
+        name: 'Diminished Scale',
+        images: diminishedScale,
+        showModes: false
+    }
+};
+
+export default function ScalesPage() {
     const [selectedScale, setSelectedScale] = useState('major-scale');
+
+    // Split images into two rows this is done to ensure that the images are displayed in two rows
+    const currentScaleImages = scalesData[selectedScale].images;
+    const firstRow = currentScaleImages.slice(0, Math.ceil(currentScaleImages.length / 2));
+    const secondRow = currentScaleImages.slice(Math.ceil(currentScaleImages.length / 2));
+
+    const renderScaleImages = (images) => {
+        return images.map((item, index) => (
+            <ScaleImage 
+                key={index}
+                img={item.img}
+                alt={item.alt}
+                position={item.position}
+            />
+        ));
+    };
 
     return (
         <section id='parent-container' className="flex flex-col items-center justify-start mt-[100px] min-h-screen w-full snap-start">
             {/* Dropdown container */}
             <div id="filter-container" className="flex items-center justify-start gap-[100px] w-[92%] h-[60px]">
-                <select className="py-[6px] w-[160px] font-outfit-small "
-                    name="scale-dropdown" 
+                <select 
+                    className="py-[6px] w-[160px] font-outfit-small bg-[#736C12]"
                     value={selectedScale}
-                    id="scale-dropdown" 
                     onChange={(e) => setSelectedScale(e.target.value)}
                 >
-                    <option value="major-scale">Major Scale</option>
-                    <option value="harmonic-minor-scale">Harmonic Minor Scale</option>
-                    <option value="natural-minor-scale">Natural Minor Scale</option>
-                    <option value="major-pentatonic-scale">Major Pentatonic Scale</option>
-                    <option value="minor-pentatonic-scale">Minor Pentatonic Scale</option>
-                    <option value="persian-scale">Persian Scale</option>
-                    <option value="diminished-scale">Diminished Scale</option>
+                    {
+                        Object.entries(scalesData).map(([key, { name }]) => (
+                            <option key={key} value={key}>{name}</option>
+                        ))
+                    }
                 </select>
 
                 {/* Show only if filter value is major scale */}
-                {selectedScale === 'major-scale' && (
-                    <select 
-                        className="py-[6px] w-[160px] font-outfit-small"
-                        name="mode-dropdown" 
-                        id="mode-dropdown"
-                    >
+                {
+                    scalesData[selectedScale].showModes && (
+                    <select className="py-[6px] w-[160px] font-outfit-small bg-[#736C12]">
                         <option value="">Select Modes</option>
                         <option value="modes">7 Modes</option>
                     </select>
-                )}
+                    )
+                }
             </div>
 
             {/* Content container */}
-            <div className="w-[92%] flex flex-col items-center justify-center gap-[20px] mt-[60px]">
+            <div className="w-[92%] flex flex-col items-center justify-center gap-[20px] mt-[20px]">
                 {/* First row */}
                 <div className="flex gap-[20px]">
-                    {
-                        firstMajorRow.map((item, index) => (
-                            <ScaleImage 
-                                key={index}
-                                img={item.img}
-                                alt={item.alt}
-                            />
-                        ))
-                    }
+                    {renderScaleImages(firstRow)}
                 </div>
-                
+
                 {/* Second row */}
                 <div className="flex gap-[20px] justify-center">
-                    {
-                        secondMajorRow.map((item, index) => (
-                            <ScaleImage 
-                                key={index}
-                                img={item.img}
-                                alt={item.alt}
-                            />
-                        ))
-                    }
+                    {renderScaleImages(secondRow)}
                 </div>
             </div>
         </section>
